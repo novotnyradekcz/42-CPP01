7236eca83859d38bbbae3214ded577869b01e9fc
Radek Novotny <novotnyradekcz@gmail.com>
2025-05-17 15:50:34 +0200

    Added ex06

---
+++ ex06/Harl.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 12:20:22 by rnovotny          #+#    #+#             */
+/*   Updated: 2025/05/17 15:42:38 by rnovotny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+	std::cout << "Harl constructor called." << std::endl;
+}
+
+Harl::~Harl()
+{
+	std::cout << "Harl destructor called." << std::endl;
+}
+
+void Harl::debug()
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info()
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void Harl::warning()
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void Harl::error()
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*functions[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	switch (level[0])
+	{
+		case 'D':
+			(this->*functions[0])();
+		case 'I':
+			(this->*functions[1])();
+		case 'W':
+			(this->*functions[2])();
+		case 'E':
+			(this->*functions[3])();
+			break;
+		default:
+			std::cout << "Invalid level." << std::endl;
+			break;
+	}
+}
--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 12:22:38 by rnovotny          #+#    #+#             */
-/*   Updated: 2025/05/17 15:48:37 by rnovotny         ###   ########.fr       */
+/*   Updated: 2025/05/17 15:45:34 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int main(int argc, char **argv)
 	}
 	else
 	{
+		std::cout << "[ " << argv[1] << " ]" << std::endl;
 		harl.complain(argv[1]);
 	}
 
