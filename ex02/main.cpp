621b9fe975492d108f4835e4010643d2be679999
Radek Novotny <novotnyradekcz@gmail.com>
2025-05-17 14:29:05 +0200

    Improved ex02 output

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:38:24 by rnovotny          #+#    #+#             */
-/*   Updated: 2025/05/12 10:42:56 by rnovotny         ###   ########.fr       */
+/*   Updated: 2025/05/17 14:28:00 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ int	main()
 	std::string	*stringPTR = &str;
 	std::string	&stringREF = str;
 
-	std::cout << "Address of string variable: " << &str << std::endl;
-	std::cout << "Address of stringPTR: " << stringPTR << std::endl;
-	std::cout << "Address of stringREF: " << &stringREF << std::endl;
-	std::cout << "Value of string variable: " << str << std::endl;
-	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
-	std::cout << "Value of stringREF: " << stringREF << std::endl;
+	std::cout << "Address of string variable: &str = " << &str << std::endl;
+	std::cout << "Address of string pointer: &stringPTR = " << &stringPTR << std::endl;
+	std::cout << "Address of string reference: &stringREF = " << &stringREF << std::endl;
+	std::cout << "Value of string variable: str = " << str << std::endl;
+	std::cout << "Value of string pointer: stringPTR = " << stringPTR << std::endl;
+	std::cout << "Value string pointer points to: *stringPTR = " << *stringPTR << std::endl;
+	std::cout << "Value of string reference: stringREF = " << stringREF << std::endl;
 }
