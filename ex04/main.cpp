082b9af2b7b52dd06b99dd4422c915ee52ee0f71
Radek Novotny <novotnyradekcz@gmail.com>
2025-05-23 18:08:42 +0200

    Removed replace function

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 12:05:45 by rnovotny          #+#    #+#             */
-/*   Updated: 2025/05/17 15:12:43 by rnovotny         ###   ########.fr       */
+/*   Updated: 2025/05/23 18:07:23 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int	main(int argc, char **argv)
 		std::cerr << "Usage: ./replace <filename> <s1> <s2>" << std::endl;
 		return (1);
 	}
+	if (argv[2][0] == '\0')
+		return (0);
 
 	std::ifstream infile(argv[1]);
 	if (!infile)
@@ -28,9 +30,10 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 
-	std::string line;
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
+	std::string line;
+	std::string end;
 	std::string output;
 
 	while (std::getline(infile, line))
@@ -38,7 +41,9 @@ int	main(int argc, char **argv)
 		size_t pos = 0;
 		while ((pos = line.find(s1, pos)) != std::string::npos)
 		{
-			line.replace(pos, s1.length(), s2);
+			end = line.substr(pos + s1.length());
+			line.erase(pos);
+			line += s2 + end;
 			pos += s2.length();
 		}
 		output += line + "\n";
